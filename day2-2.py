67c9d9ffa34a7681a1c9aafebbd193864ae27146
slothgsr <dustin.rego@gmail.com>
2020-12-02 16:26:03 -0500

    day2-2 done. fixed up day2-1

--- day2-2.py
+++ day2-2.py
@@ -3,6 +3,9 @@ def convert(line):
     Dashpos = line.find('-')
     letterpos = line.find(':') - 1
     letter = line[letterpos]
+    splitline = line.split(' ')
+    password = splitline[2]
+
     if Dashpos == 1:
         num1= int(line[0])
         if line[3] == ' ':
@@ -12,8 +15,11 @@ def convert(line):
     if Dashpos == 2:
         num1 = int(line[0:2])
         num2 = int(line[3:5])
-    lettercount = line.count(letter) -1
-    if lettercount >= num1 and lettercount <= num2:
+
+    if password[num1 -1] == letter and password[num2 -1] != letter:
+        correct += 1
+
+    elif password[num1 -1] != letter and password[num2 -1] == letter:
         correct += 1
 
 file = (r'C:\Users\SlothGSR\Desktop\python_stuff\MyPythonScripts\Advent2020\Day2\pws.txt')
