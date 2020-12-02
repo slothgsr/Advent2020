08ad565134f16a899b91c6300e236354e5e94a3b
slothgsr <dustin.rego@gmail.com>
2020-12-02 09:33:37 -0500

    fixed days

---
+++ Day1/day1-1.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+file = (r'C:\Users\SlothGSR\Desktop\python_stuff\MyPythonScripts\Advent2020\Day1\numbers.txt')
+
+with open(file) as f:
+    lines = [int(line.rstrip('\n')) for line in f]
+
+combos = combinations(lines, 2)
+
+for i in combos:
+    if sum(i) == 2020:
+        print(i[0] * i[1])
+
+                
+
--- Day1/day1-2.py
+++ Day1/day1-2.py
@@ -1,6 +1,8 @@
 from itertools import combinations
 
-with open("numbers.txt") as f:
+file = (r'C:\Users\SlothGSR\Desktop\python_stuff\MyPythonScripts\Advent2020\Day1\numbers.txt')
+
+with open(file) as f:
     lines = [int(line.rstrip('\n')) for line in f]
 
 combos = combinations(lines, 3)
@@ -8,6 +10,3 @@ combos = combinations(lines, 3)
 for i in combos:
     if sum(i) == 2020:
         print(i[0] * i[1] * i[2])
-
-                
-
