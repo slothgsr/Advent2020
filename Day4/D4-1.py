029a56b6db0ffff6f8dc373c84fa199615c526df
slothgsr <dustin.rego@gmail.com>
2020-12-04 15:51:11 -0500

    D4-1 off by 1

---
+++ Day4/D4-1.py
@@ -0,0 +1,27 @@
+import os
+import itertools
+
+file = (r'C:\Users\SlothGSR\Desktop\python_stuff\MyPythonScripts\Advent2020\Day4\input.txt')
+
+with open(file) as f:
+    space = 1
+    wordlist =[]
+    templist =[]
+    for line in f.readlines():
+        if line == "\n":
+                wordlist.append(templist)
+                templist = []
+        for word in line.split():
+            templist.append(word)
+
+musthave=['byr:','iyr:','eyr:','hgt:','hcl:','ecl:','pid:']
+positive = 0
+for group in wordlist:
+    hit = 0
+    for subgroup in group:
+        if subgroup[0:4] in musthave:
+            hit += 1
+    if hit >= 7:
+         positive += 1
+        
+print(positive)
