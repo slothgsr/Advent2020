a201382abe336209a27acecba96d3c6033de4133
slothgsr <dustin.rego@gmail.com>
2020-12-07 15:30:50 -0500

    D5 DONE!

--- Day5/Day5-1.py
+++ Day5/Day5-1.py
@@ -1,32 +1,44 @@
 #!/usr/bin/env python
 import os
-import bisect 
 
 os.chdir((os.path.dirname(os.path.realpath(__file__))))
 
-file = ("test.txt")
+file = ("input.txt")
 
 with open(file) as f:
     tickets = [line.rstrip('\n') for line in f]
 
+seats = []
 for ticket in tickets:
     rowsmin = 0
     rowsmax = 127
     columnsmax = 7
     columnsmin = 0
-    print("ticket")
-    print(ticket[0])
-
     for letter in ticket[0:7]:
-        print(letter)
         if letter == "F":
             rowsmax = rowsmax - (rowsmax - rowsmin + 1) /2
-            # print("max:",rowsmax)
-            # print("min:",rowsmin)
         if letter == "B":
             rowsmin = rowsmin + (rowsmax - rowsmin + 1) /2
-            # print("max:",rowsmax)
-            # print("min:",rowsmin)
 
-    print(rowsmin, rowsmax)
+    row = int(rowsmin)
+
+    for letter in ticket[7:]:
+        if letter == "L":
+            columnsmax = columnsmax - (columnsmax - columnsmin + 1) /2
+        if letter == "R":
+            columnsmin = columnsmin + (columnsmax - columnsmin + 1) /2
+
+    column = int(columnsmin)
+
+    seats.append(row * 8  + column)
+
+print("Max seat ID:", sorted(seats)[-1])
+
+start = 71
+for i in sorted(seats):
+    if int(i)  != start:
+        print("Missing Seat ID:", i-1)
+        start += 1
+    start += 1
+
 
