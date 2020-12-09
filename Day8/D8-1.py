4ad94bc0d83f0ff81877188afbf8b88fac343651
slothgsr <dustin.rego@gmail.com>
2020-12-09 15:21:39 -0500

    D8 wip

--- Day8/D8-1.py
+++ Day8/D8-1.py
@@ -2,37 +2,50 @@ import os
 
 os.chdir((os.path.dirname(os.path.realpath(__file__))))
 
-file = ("input.txt")
+file = ("sample.txt")
 
 with open(file) as f:
     operations = [(line.rstrip('\n')) for line in f]
 
-
 label = 1
-
 labeledlist = []
-
 for i in operations:
-    labeledlist.append(f"{i} ,line:{label}: ")
+    labeledlist.append(f"{label} {i}, ")
     label += 1
 
 print(labeledlist)
 
-# acc = 0
-# nextline = 0
-# used = []
-# for i in range(len(operations)):
+checking = True
+used = []
+start = 0
+currentline = 0
+nextline = 0
+acc = 0
 
-#     if operations[i][0:3] == "nop":
-#         nextline = i+1
-#         used.append(i)
+
+
+
     
-        
+while checking:
+    if labeledlist[currentline] in used:
+        print(acc)
+        checking = False
+    used.append(labeledlist[currentline])
+    splitlines = labeledlist[currentline].split(" ")
+    print(splitlines)
+    if splitlines[1] == "nop":
+        currentline += 1
+    if splitlines[1] == "acc":
+        if splitlines[1[0]] == "+":
+            acc += int(splitlines[2])
+        else:
+            acc -= int(splitlines[2])
+        currentline += 1 
+    if splitlines[1] == "jmp":
+        if splitlines[1[0]] == "+":
+            currentline += int(splitlines[2])
+        else:
+            currentline -= int(splitlines[2])
 
-print(len(operations))
-
-#TODO number each line
-#TODO iterate operations
-#TODO Log what line is being called.
-#TODO if repeat.  return acc number
+    
 
